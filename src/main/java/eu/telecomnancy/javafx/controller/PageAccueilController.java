a51022fc13207e2145b42193aafa8ff8437142a6
ABDOULHOUSSEN Hamza <hamza.abdoulhoussen@telecomnancy.eu>
2021-12-13 15:47:09 +0100

    Ajout PageAccueilController

---
+++ src/main/java/eu/telecomnancy/javafx/controller/PageAccueilController.java
@@ -0,0 +1,28 @@
+package eu.telecomnancy.javafx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class PageAccueilController {
+    
+    @FXML
+    private TextField input;
+
+    @FXML
+    private Label erreur;
+
+    public PageAccueilController() {}
+
+    @FXML
+    protected void Connection() {}
+
+    @FXML
+    protected void Professeur() {}
+
+    @FXML
+    protected void Etudiant() {}
+
+}
