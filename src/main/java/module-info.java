9466dd0dd45dca9001de191d737b3ba06f4b875c
Sami Boulechfar <sami.boulechfar@telecomnancy.eu>
2021-12-14 15:25:17 +0100

    Ajout du driver pour SQL

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module eu.telecomnancy.javafx {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.desktop;
+    requires java.sql;
     opens eu.telecomnancy.javafx to javafx.fxml;
     opens eu.telecomnancy.javafx.controller to javafx.fxml;
 
