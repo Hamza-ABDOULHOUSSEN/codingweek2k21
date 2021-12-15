59fac8e4247b4b8cdd0350ad3fe4bef1ff97ee29
Sami Boulechfar <sami.boulechfar@telecomnancy.eu>
2021-12-15 18:00:03 +0100

    Modification des controller

--- src/main/java/eu/telecomnancy/javafx/controller/PageDemandeRdvController.java
+++ src/main/java/eu/telecomnancy/javafx/controller/PageDemandeRdvController.java
@@ -6,12 +6,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 import java.io.IOException;
 
 public class PageDemandeRdvController implements Observateur {
 
     private MyRdv myrdv;
+
+    @FXML private MenuButton choisirProf ;
+    @FXML private MenuButton choisirCreneau ;
 
     public PageDemandeRdvController(MyRdv myrdv) {
         this.myrdv = myrdv;
@@ -29,6 +34,32 @@ public class PageDemandeRdvController implements Observateur {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         myrdv.setScene(scene);
+    }
+
+    public void initChoixProf() {
+        for (int i : myrdv.getConnect().getGestionnaireProf().getTable_prof().keySet()) {
+            MenuItem mi = new MenuItem() ;
+            mi.setText(myrdv.getConnect().getGestionnaireProf().getTable_prof().get(i).getNom() + " " + myrdv.getConnect().getGestionnaireProf().getTable_prof().get(i).getPrenom());
+            mi.setOnAction(e -> { setChoixProf(mi.getText()) ; });
+            choisirProf.getItems().add(mi) ;
+        }
+    }
+
+    public void initChoixHoraire() {
+        for (int i : myrdv.getConnect().getGestionnaireCreneau().getTable_creneau().keySet()) {
+            MenuItem mi = new MenuItem() ;
+            mi.setText(myrdv.getConnect().getGestionnaireCreneau().getTable_creneau().get(i).getJour() + " " + myrdv.getConnect().getGestionnaireCreneau().getTable_creneau().get(i).getHeure());
+            mi.setOnAction(e -> { setChoixHoraire(mi.getText()) ; });
+            choisirCreneau.getItems().add(mi) ;
+        }
+    }
+
+    public void setChoixProf(String text) {
+        this.choisirProf.setText(text);
+    }
+
+    public void setChoixHoraire(String text) {
+        this.choisirCreneau.setText(text);
     }
 
     @Override public void update() {
