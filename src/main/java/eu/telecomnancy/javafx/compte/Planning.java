1a5456bd67c36a29fb574a37b4b22ae307186a16
Sami Boulechfar <sami.boulechfar@telecomnancy.eu>
2021-12-15 14:13:30 +0100

    Modification des classes du package Compte

--- src/main/java/eu/telecomnancy/javafx/compte/Planning.java
+++ src/main/java/eu/telecomnancy/javafx/compte/Planning.java
@@ -3,9 +3,18 @@ package eu.telecomnancy.javafx.compte;
 import eu.telecomnancy.javafx.rdv.RendezVous;
 
 public class Planning {
-    public Planning() {}
+    private int id_creneau ;
+    private int id_enseignant ;
+
+    public Planning(int id_creneau, int id_enseignant) {
+        this.id_creneau = id_creneau ;
+        this.id_enseignant = id_enseignant ;
+    }
 
     public void addRdvToPlanning(RendezVous rdv) {
+    }
 
+    public int getId_planning() {
+        return this.id_creneau ;
     }
 }
--- src/main/java/eu/telecomnancy/javafx/compte/Professeur.java
+++ src/main/java/eu/telecomnancy/javafx/compte/Professeur.java
@@ -4,9 +4,13 @@ public class Professeur extends Compte {
     private int id_professeur;
     private Planning planning ;
 
-    public Professeur(int id_professeur, String mdp, String nom, String prenom, String email, int tel, String adresse) {
+    public Professeur(int id_professeur, String mdp, String nom, String prenom, String email, String tel, String adresse) {
         super(id_professeur, mdp, nom, prenom, email, tel, adresse);
         this.id_professeur = id_professeur ;
+    }
+
+    public int getId_professeur() {
+        return id_professeur;
     }
 
     public Planning getPlanning() {
