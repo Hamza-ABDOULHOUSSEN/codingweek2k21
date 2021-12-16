e9c9699b9d91a453d8445e546b6664ff4ce4a087
Maha Khatib <Maha.Khatib@telecomnancy.eu>
2021-12-16 15:09:46 +0100

    ajout du controller de la page profil de l'élève

--- src/main/java/eu/telecomnancy/javafx/controller/PageProfilEleveController.java
+++ src/main/java/eu/telecomnancy/javafx/controller/PageProfilEleveController.java
@@ -1,4 +1,62 @@
 package eu.telecomnancy.javafx.controller;
 
-public class PageProfilEleveController {
+import eu.telecomnancy.javafx.Observateur.Observateur;
+import eu.telecomnancy.javafx.model.MyRdv;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PageProfilEleveController implements Observateur {
+
+    @FXML private TextField inputNom ;
+    @FXML private TextField inputPrenom ;
+    @FXML private TextField inputEmail ;
+    @FXML private TextField inputTel ;
+    @FXML private TextField inputAdresse ;
+    @FXML private TextField inputMotdepasse ;
+
+
+    @FXML public void modifierNom() {}
+    @FXML public void modifierPrenom() {}
+    @FXML public void modifierEmail() {}
+    @FXML public void modifierTel() {}
+    @FXML public void modifierAdresse() {}
+    @FXML public void modifierMotdepasse() {}
+    @FXML public void AfficherMdp() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void update() {
+
+    }
 }
